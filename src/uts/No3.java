271e8c11f336ea78ff9111a67305177bfbfe4580
Eka Nurul <145452281+ekaaaanrllllll@users.noreply.github.com>
2024-04-22 19:33:10 +0700

    Update No3.java

--- src/uts/No3.java
+++ src/uts/No3.java
@@ -1,3 +1,22 @@
+/*
+EKA NURUL BARIDAH (2206789)
+PENDIDIKAN ILMU KOMPUTER A
+
+ SOAL 3 :
+(Jumlah digit dalam bilangan bulat) Tulis metode yang menghitung jumlah digit dalam 
+bilangan bulat. Gunakan tajuk metode berikut :
+public static int sumDigits(long n)
+
+contoh :
+Misalnya, sumDigits(234) mengembalikan 9 (2 + 3 + 4). (Petunjuk: Gunakan operator 
+% untuk mengekstrak digit, dan operator / untuk menghapus digit yang diekstraksi. 
+Misalnya, untuk mengekstrak 4 dari 234, gunakan 234 % 10 (= 4). Untuk menghapus 
+4 dari 234, gunakan 234 / 10 ( = 23). Gunakan loop untuk mengekstrak dan 
+menghapus digit berulang kali hingga semua digit terekstraksi. Tulis program 
+pengujian yang meminta pengguna memasukkan bilangan bulat dan menampilkan 
+jumlah semua digitnya.
+*/
+
 import java.util.Scanner;
 
 public class No3 {
